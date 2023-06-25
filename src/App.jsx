a774bfb429d16267634af8fccff43681893c4041
arshad292 <arshad.kma@gmail.com>
2023-06-25 23:46:06 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Home from './Components/home/Home'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <main className='main'>
+      <Home/>
+    </main>
+  )
+}
+
+export default App
+ 
